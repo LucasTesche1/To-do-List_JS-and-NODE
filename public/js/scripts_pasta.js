6da61ab60c6a53f0556ecf48328fb1cd573e1140
LucasTesche1 <lucas@yujilive.com.br>
2024-06-04 14:45:06 -0300

    Precisa-se incluir a nova funcionalidade das pastas

---
+++ public/js/scripts_pasta.js
@@ -0,0 +1,11 @@
+function redirecionaIndex1() {
+  return (window.location.href = "/views/index.html");
+}
+
+function redirecionaIndex2() {
+  return (window.location.href = "/views/index2.html");
+}
+
+function redirecionaIndex3() {
+  return (window.location.href = "/views/index3.html");
+}
