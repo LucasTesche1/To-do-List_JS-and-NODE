1397626bee3c4708a9680874d94a4b6e07dfa2a1
LucasTesche1 <lucas@yujilive.com.br>
2024-06-10 10:49:07 -0300

    CSS PRONTO

--- public/js/scripts_pasta.js
+++ public/js/scripts_pasta.js
@@ -1,4 +1,3 @@
-
 function redirecionaIndex1() {
   return (window.location.href = "/views/index.html");
 }
@@ -9,4 +8,23 @@ function redirecionaIndex2() {
 
 function redirecionaIndex3() {
   return (window.location.href = "/views/index3.html");
+}
+
+function editarNomePasta(button) {
+  const container = button.parentElement;
+  const inputContainer = container.querySelector('.edit-input-container');
+  inputContainer.style.display = 'flex';
+}
+
+function salvarNomePasta(button) {
+  const container = button.parentElement.parentElement;
+  const input = container.querySelector('.edit-input');
+  const newName = input.value.trim();
+
+  if (newName) {
+    const folderNameSpan = container.querySelector('.folder-name');
+    folderNameSpan.textContent = newName;
+  }
+
+  button.parentElement.style.display = 'none';
 }
